1bf12d5e2e0ec5deffc514a3060558c1ecd24fc2
JalolDev <verve.neowise@gmail.com>
2022-01-18 21:19:59 +0500

    First commit

---
+++ app/src/main/java/ohohohoh/xaxaxaxaxax/cubics/MainActivity.kt
@@ -0,0 +1,75 @@
+package ohohohoh.xaxaxaxaxax.cubics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getColorStateList
+import ohohohoh.xaxaxaxaxax.cubics.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val colors = arrayOf(R.color.red, R.color.blue, R.color.green, R.color.yellow)
+    private val buttons = Array(25) { 0 }
+
+    private lateinit var binding: ActivityMainBinding
+    private var requiredColor: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        requiredColor = colors.random()
+        binding.indicator.backgroundTintList = getColorStateList(this, requiredColor)
+
+        val factor = binding.root.context.resources.displayMetrics.density
+
+        binding.grid.rowCount = 3
+        binding.grid.columnCount = 3
+
+        for(i in 0..8) {
+            val button = AppCompatButton(this)
+            val layoutParams = ViewGroup.LayoutParams((60 * factor).toInt(), (60 * factor).toInt())
+
+            button.layoutParams = layoutParams
+            button.requestLayout()
+
+            button.randomColor(i)
+
+            button.setOnClickListener {
+                button.randomColor(i)
+                check()
+            }
+
+            binding.grid.addView(button)
+        }
+    }
+
+    private fun check() {
+        if (buttons.all { it == requiredColor }) {
+            Toast.makeText(this, "You win!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun AppCompatButton.randomColor(i: Int) {
+        val color = colors.random()
+        buttons[i] = color
+        this.backgroundTintList = getColorStateList(this@MainActivity, color)
+    }
+}
+
+/*
+
+https://<username>:<token>@<repo-url>
+
+Remote:
+    https://verve-neowise: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@github.com/verve-neowise/cubics-android.git
+
+username:
+token:
+repo:
+
+ */
